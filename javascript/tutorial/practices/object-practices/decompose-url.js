5b230211634d796d0351e9835378bacb11a93e4d
zeynepdinc <zeynep.dinc@udemy.com>
2021-01-04 14:27:34 +0300

    improve

--- javascript/tutorial/practices/object-practices/decompose-url.js
+++ javascript/tutorial/practices/object-practices/decompose-url.js
@@ -1,21 +1,29 @@
 function decomposeUrl(url){
+    let protocol, ipAddress, subdomain, domainName, folderTree,targetFile, argumentsFile = null;
     let arr = url.split(":");
-    let protocol = arr[0];
-    let argumentsFile = arr[1].substr(arr[1].search("\\?"),arr[1].length);
+    if (url.includes(":") && typeof  isNaN(arr[0])){
+        protocol = arr[0];
+    }
     let domainString = arr[1].substr(0,arr[1].search("\\?"));
-    let domainArr = domainString.split("/");
-    let subdomain =  domainArr[0].substr(0,domainArr[0].search("\\."));
-    let domainName = domainArr[0].substr(domainArr[0].search("\\.")+1, domainArr[0].length);
-    let folderTree = [];
-    let targetFile = null;
-    for(let i = 1; i < domainArr.length; i++){
-        if(i==domainArr.length-1 && domainArr[i].match("\\.")){
-            targetFile = domainArr[i];
-        }
-        else{
-            folderTree.push(domainArr[i]);
+    if(0){
+        ipAddress = domainString;
+    }
+    else {
+        argumentsFile = arr[1].substr(arr[1].search("\\?"), arr[1].length);
+        let domainArr = domainString.split("/");
+        subdomain = domainArr[0].substr(0, domainArr[0].search("\\."));
+        domainName = domainArr[0].substr(domainArr[0].search("\\.") + 1, domainArr[0].length);
+        folderTree = [];
+        targetFile = null;
+        for (let i = 1; i < domainArr.length; i++) {
+            if (i == domainArr.length - 1 && domainArr[i].match("\\.")) {
+                targetFile = domainArr[i];
+            } else {
+                folderTree.push(domainArr[i]);
+            }
         }
     }
+
     return {
             protocol: protocol,
             ipAddress: null,
@@ -27,5 +35,5 @@ function decomposeUrl(url){
     }
 }
 
-console.log(decomposeUrl("https:www.google.com/asd/asd/test.js?ok=1"));
-console.log(decomposeUrl("https:www.google.com/asd/asd/test?ok=1"));
+console.log(decomposeUrl("https:www.google.com//asd/asd/test.js?ok=1"));
+// console.log(decomposeUrl("https:www.google.com/asd/asd/test?ok=1"));
