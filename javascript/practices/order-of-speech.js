786d3fbc556fb73043e8c01a847e267ee5d97079
zeynepdinc <zeynep.dinc@udemy.com>
2021-01-12 14:10:51 +0300

    practice

---
+++ javascript/practices/order-of-speech.js
@@ -0,0 +1,13 @@
+
+let names =  ["Bilal", "Ilker", "Mehmet", "Sabri", "Selin", "Ufuk", "Volkan", "Zeynep"];
+let nameLength = names.length;
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+for(let i=nameLength-1; i > 0; i--){
+    let randomIndex = getRndInteger(0,i);
+    console.log(names[randomIndex]);
+    names.splice(randomIndex, 1);
+}
