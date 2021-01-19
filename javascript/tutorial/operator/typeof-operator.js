bff155a97081049c6e70c9240bc19c1f98711cb8
zeynepdinc <zeynep.dinc@udemy.com>
2021-01-19 18:10:02 +0300

    typeof

---
+++ javascript/tutorial/operator/typeof-operator.js
@@ -0,0 +1,8 @@
+console.log(typeof 3);                          // number
+console.log(typeof 4.5);                        // number
+console.log(typeof true);                       // boolean
+console.log(typeof false);                      // boolean
+console.log(typeof null);                       // object -> bug
+console.log(typeof undefined);                  // undefined
+console.log(typeof {});                         // object
+console.log(typeof function f(){});             // function but we should think special object.
